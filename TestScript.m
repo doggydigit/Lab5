706ffc10c171e63407abb461ca17a1d8d4926057
doggydigit <matthias.tsai@epfl.ch>
2017-04-08 15:47:31 +0200

    ABC mostly done. D and E to do

--- TestScript.m
+++ TestScript.m
@@ -1,6 +1,8 @@
+close all
+
 %% Initialize 
-times = 0:0.01:20;
-drives = linspace(2, 2, length(times));
+times = 0:0.01:40;
+drives = linspace(5, 0, length(times));
 
 %% Add white noise to drive
 % sigma = linspace(0,4,length(times));
@@ -10,5 +12,9 @@ drives = linspace(2, 2, length(times));
 
 %% Simulate and Plot
 %sigma = [zeros(ceil(length(times)/4),1)', linspace(0,0.9,floor(length(times)*3/4))];
-[theta, r, x1, dtheta] = salam_cpg_osc(times, drives);
-%plot_salam_cpg(times, x, dtheta, drives);
+[theta, r, x, dtheta] = salam_cpg_osc(times, drives);
+xl = x;
+xr = [x(:,9:16),x(:,1:8),x(:,18),x(:,17),x(:,20),x(:,19)];
+%plot_salam_cpg(times, xl, dtheta, drives);
+%plot_salam_cpg(times, xr, dtheta, drives);
+plot_all_oscillators(times,x,dtheta,drives);
